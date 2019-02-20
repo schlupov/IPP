edc4d3fd2e9cea9d924db391b86d373597005f19
Silvie Chlupova <sisi.chlupova@gmail.com>
2019-02-20 18:41:38 +0100

    Minor changes because of labels

--- CreateXML.php
+++ CreateXML.php
@@ -25,19 +25,34 @@ class CreateXML {
                     $instructionNode->setAttribute("order", $counter);
                 }
                 $instructionNode->setAttribute("opcode", $instruction);
+                if (($this -> forXML[$i][$c][0] == "CALL") || ($this -> forXML[$i][$c][0] == "JUMP") ||
+                    ($this -> forXML[$i][$c][0] == "JUMPIFEQ") || ($this -> forXML[$i][$c][0] == "JUMPIFNEQ")) {
+                    foreach ($this -> forXML[$i+1] as $p => $g) {
+                        $literal = $this->forXML[$i+1][$p][0];
+                    }
+                    $callCounter++;
+                    $flag = 0;
+                    $instructionNode->appendChild($this->PrepareArgument($flag, $literal, $domTree, $this -> forXML[$i], $callCounter));
+                }
                 if (($c != "KEYWORD") && ($c == "DIGIT" || $c == "STRING" || $c == "true" ||
                     $c == "false" || $c == "nil")){
                     $literal = $this -> forXML[$i][$c][0];
+                    $flag = 1;
                     $callCounter++;
-                    $flag = true;
-                    $instructionNode->appendChild($this->PrepareArgument($flag, $literal, $domTree, $this -> forXML[$i], $callCounter));
+                    $node = $this->PrepareArgument($flag, $literal, $domTree, $this->forXML[$i], $callCounter);
+                    if ($node != null) {
+                        $instructionNode->appendChild($node);
+                    }
+                    else {
+                        $callCounter--;
+                    }
                 }
                 if ($this -> forXML[$i][$c][0] == "READ") {
                     foreach ($this -> forXML[$i+2] as $p => $g) {
                         $literal = $this->forXML[$i+2][$p][0];
                     }
                     $callCounter++;
-                    $flag = false;
+                    $flag = 2;
                     $instructionNode->appendChild($this->PrepareArgument($flag, $literal, $domTree, $this -> forXML[$i], $callCounter));
                 }
                 $root->appendChild($instructionNode);
@@ -51,7 +66,7 @@ class CreateXML {
     }
 
     private function PrepareArgument ($flag, $literal, $domTree, $type, $callCounter) {
-        if ($flag) {
+        if ($flag == 1) {
             foreach ($type as $c => $z) {
                 switch ($c) {
                     case "int":
@@ -80,26 +95,31 @@ class CreateXML {
                         $literal = "LF" . "@" . $literal;
                         break;
                     default:
-                        $atr = "label";
+                        $atr = "";
                         break;
                 }
             }
         }
-        else {
+        elseif ($flag == 2) {
             $atr = "type";
         }
-        if ($callCounter == 1) {
-            $node = $domTree->createElement("arg1");
-        }
-        elseif ($callCounter == 2) {
-            $node = $domTree->createElement("arg2");
-        }
         else {
-            $node = $domTree->createElement("arg3");
+            $atr = "label";
         }
-        $node->setAttribute("type", $atr);
-        $node->textContent = $literal;
-        return $node;
+        if ($atr != "") {
+            if ($callCounter == 1) {
+                $node = $domTree->createElement("arg1");
+            }
+            elseif ($callCounter == 2) {
+                $node = $domTree->createElement("arg2");
+            }
+            else {
+                $node = $domTree->createElement("arg3");
+            }
+            $node->setAttribute("type", $atr);
+            $node->textContent = $literal;
+            return $node;
+        }
     }
 
     function __destruct() {
